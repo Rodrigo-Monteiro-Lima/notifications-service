68b7aa2f3942b4beef80ea5005d6df19359a8443
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2022-12-30 22:45:19 -0300

    feat: prisma repository

---
+++ src/infra/database/prisma/repositories/prisma-notifications-repository.ts
@@ -0,0 +1,19 @@
+import { Notification } from 'src/application/entities/notification';
+import { NotificationsRepository } from '../../../../../src/application/repositories/notifications-repository';
+import { PrismaService } from '../prisma.service';
+
+export class PrismaNotificationsRepository implements NotificationsRepository {
+  constructor(private prisma: PrismaService) {}
+  async create(notification: Notification): Promise<void> {
+    await this.prisma.notification.create({
+      data: {
+        id: notification.id,
+        category: notification.category,
+        content: notification.content.value,
+        recipientId: notification.recipientId,
+        readAt: notification.readAt,
+        createdAt: notification.createdAt,
+      },
+    });
+  }
+}
